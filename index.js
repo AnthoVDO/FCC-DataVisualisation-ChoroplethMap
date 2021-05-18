82b1e1370c37e04ef15cdc90684973107e64468f
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-05-18 17:41:19 +0200

    I finished the project

--- index.js
+++ index.js
@@ -77,6 +77,22 @@ const callMap = async(urlMap, urlDegree) => {
         return idDegree;
     }
 
+    //function to attribute area
+
+    const checkArea = (id) => {
+        const result = degree.filter(e => e.fips === id);
+        const idArea = result[0].area_name;
+        return idArea;
+    }
+
+    //function to attribute state
+
+    const checkState = (id) => {
+        const result = degree.filter(e => e.fips === id);
+        const idState = result[0].state;
+        return idState;
+    }
+
 
     //Create the map
 
@@ -93,33 +109,67 @@ const callMap = async(urlMap, urlDegree) => {
         .attr("transform", "scale(0.8, 0.8)")
         .attr("class", "county")
         .style("fill", d => checkColor(d.id))
-        .attr("class", "county")
         .attr("data-fips", d => d.id)
         .attr("data-education", d => checkDegree(d.id))
+        .attr("data-area", d => checkArea(d.id))
+        .attr("data-state", d => checkState(d.id))
 
     map.attr("transform", "translate(30, 10)")
 
 
     /*-------------------------------------Legend-------------------------------------*/
     //Legend's scale
-    const yScale = d3.scaleBand().domain(COLOR__LEGEND__VALUE.map(x => x)).range(CHART__HEIGHT - MARGIN.bottom, MARGIN.top)
+
+    const COLOR__LEGEND__VALUE__SCALE = d3.range(minDegree, maxDegree, (maxDegree - minDegree) / 6);
+
+    const yScale = d3.scaleBand().domain(COLOR__LEGEND__VALUE__SCALE).range([CHART__HEIGHT - MARGIN.bottom, MARGIN.top])
+    const yScaleTick = d3.scaleLinear().domain([minDegree, maxDegree]).range([CHART__HEIGHT - MARGIN.bottom, MARGIN.top])
+
     const legend = SVG.append("g")
     legend.attr("id", "legend")
         .selectAll("rect")
-        .data(COLOR__LEGEND__VALUE)
+        .data(COLOR__LEGEND__VALUE__SCALE)
         .enter()
         .append("rect")
         .attr("width", 10)
-        .attr("height", 10)
+        .attr("height", yScale.bandwidth())
         .attr("y", d => yScale(d))
         .attr("fill", d => COLOR(d))
 
 
-
+    legend.call(d3.axisRight(yScaleTick).tickFormat(x => x.toFixed(2) + "%").tickSize(10).tickValues(COLOR__LEGEND__VALUE__SCALE))
+        .attr("transform", `translate(${CHART__WIDTH-MARGIN.right})`)
 
     //create tooltip
 
+    const tooltip = d3.select(".chart__container").append("div");
+
+    tooltip.attr("id", "tooltip")
+        .style("opacity", "0")
+        .style("position", "absolute")
+        .attr("data-education", "10")
+        .style("background-color", "orange")
+        .style("padding", "10px")
+        .style("color", "#000608")
+
+
     //hover effect
+
+    map.on("mouseover", (e) => {
+        console.log(e.offsetX)
+        tooltip.style("opacity", "0.8")
+            .style("left", `${e.offsetX-80}px`)
+            .style("top", `${e.offsetY+150}px`)
+            .attr("data-education", e.target.dataset.education)
+            .html(`Bachelor or higher: ${e.target.dataset.education}<br>
+        State: ${e.target.dataset.state}<br>
+        Area: ${e.target.dataset.area}<br>
+        Fips: ${e.target.dataset.fips}`)
+    })
+
+    map.on("mouseout", () => {
+        tooltip.style("opacity", "0")
+    })
 
 
 
